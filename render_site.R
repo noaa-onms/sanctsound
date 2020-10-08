ac9bf2e332da7c642f3c724ac8f7ca735d604319
Ben Best <ben@ecoquants.com>
2020-10-07 17:15:27 -0700

    sounds div

--- render_site.R
+++ render_site.R
@@ -15,40 +15,39 @@ modal_pages <- modals %>%
 #   #filter(modal_title == "Seal bombs") %>% 
 #   filter(modal_title == "Giant sea bass") %>% 
 #   View()
-modal_pages <- filter(modal_pages, modal_title == "Seal bombs")
-
+# modal_pages <- filter(modal_pages, modal_title == "Seal bombs")
 pwalk(modal_pages, render_modal)
 
 # create/render modals by iterating over svg links in csv ----
-for (i in 1:nrow(d_modals)){ # i=5
-  # paths
-  htm <- d_modals$link[i]
-  rmd <- path_ext_set(htm, "Rmd")
-  
-  #if (htm == "modals/ca-sheephead.html") browser()
-  
-  # create Rmd, if doesn't exist
-  if (!file.exists(rmd)) file.create(rmd)
-  
-  # render Rmd to html, if Rmd newer or redoing
-  if (file.exists(htm)){
-    rmd_newer <- file_info(rmd)$modification_time > file_info(htm)$modification_time
-  } else {
-    rmd_newer <- T
-  }
-  if (rmd_newer | redo_modals){
-    render_modal(rmd)
-  }
-}
+# for (i in 1:nrow(d_modals)){ # i=5
+#   # paths
+#   htm <- d_modals$link[i]
+#   rmd <- path_ext_set(htm, "Rmd")
+#   
+#   #if (htm == "modals/ca-sheephead.html") browser()
+#   
+#   # create Rmd, if doesn't exist
+#   if (!file.exists(rmd)) file.create(rmd)
+#   
+#   # render Rmd to html, if Rmd newer or redoing
+#   if (file.exists(htm)){
+#     rmd_newer <- file_info(rmd)$modification_time > file_info(htm)$modification_time
+#   } else {
+#     rmd_newer <- T
+#   }
+#   if (rmd_newer | redo_modals){
+#     render_modal(rmd)
+#   }
+# }
 
 # render website, ie Rmds in root ----
-walk(list.files(".", "*\\.md$"), render_page)
-walk(
-  list.files(".", "*\\.html$"), 
-  function(x) file.copy(x, file.path("docs", x)))
+# walk(list.files(".", "*\\.md$"), render_page)
+# walk(
+#   list.files(".", "*\\.html$"), 
+#   function(x) file.copy(x, file.path("docs", x)))
 rmarkdown::render_site()
 
-fs::file_touch("docs/.nojekyll")
+# fs::file_touch("docs/.nojekyll")
 
 # shortcuts w/out full render:
 # file.copy("libs", "docs", recursive=T)
