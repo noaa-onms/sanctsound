a833cde9027ddd3d8b702043984c872899c356b7
Ben Best <ben@ecoquants.com>
2021-04-27 09:32:22 -0700

    + sounds & latest

--- draft/render_site.R
+++ draft/render_site.R
@@ -1,6 +1,6 @@
 source(here::here("draft/functions.R"))
 
-redo_modals <- F
+redo_modals <- T
 
 # nav menus in _site.yml ----
 
@@ -35,22 +35,14 @@ render_sanctuary <- function(code, name, type, ...){
 #sites <- sites %>% filter(code == "fknms")
 #sites <- sites %>% 
 sites %>% 
-  filter(code == "hihwnms") %>% 
+  #filter(code == "hihwnms") %>% 
+  #filter(code == "cinms") %>% 
   #filter(code == "fknms") %>% 
   pwalk(render_sanctuary)
-
-# rmarkdown render
-# File  not found in resource path
-# Error: pandoc document conversion failed with error 99
-# Called from: pandoc_convert
-
-# /Applications/RStudio.app/Contents/MacOS/pandoc/pandoc +RTS -K512m -RTS _sanctuary_template.utf8.md --to html4 --from markdown+autolink_bare_uris+tex_math_single_backslash --output s_fknms.html --email-obfuscation none --self-contained --standalone --section-divs --template /Library/Frameworks/R.framework/Versions/4.0/Resources/library/rmarkdown/rmd/h/default.html --no-highlight --variable highlightjs=1 --css libs/styles.css --include-before-body /var/folders/2r/grqvdjfn04361tzk8mh60st40000gn/T//RtmpNpYAlp/rmarkdown-str1a8356c7bd87.html --variable navbar=1 --variable body_padding=45 --variable header_padding=50 --variable 'theme:yeti' --include-in-header /var/folders/2r/grqvdjfn04361tzk8mh60st40000gn/T//RtmpNpYAlp/rmarkdown-str1a83536c1693.html --mathjax --variable 'mathjax-url:https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
-# --lua-filter /Library/Frameworks/R.framework/Versions/4.0/Resources/library/rmarkdown/rmd/lua/pagebreak.lua --lua-filter /Library/Frameworks/R.framework/Versions/4.0/Resources/library/rmarkdown/rmd/lua/latex-div.lua
 
 # TODO: make update_sites_menu() so menu could be dynamic 
     
 # modals ----
-
 modals    <- get_sheet("modals", redo = redo_modals)
 modal_pages <- modals %>% 
   group_by(sanctuary_code, modal_title) %>% 
