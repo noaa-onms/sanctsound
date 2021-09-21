592b79ab20a4c74d08f1ff608149a64f5cf79e9c
Ben Best <ben@ecoquants.com>
2021-09-21 13:08:04 -0700

    updated packages; renv::snapshot(); map showing #6

--- draft/render_site.R
+++ draft/render_site.R
@@ -5,7 +5,7 @@ if (!require("librarian")){
 shelf(here)
 source(here::here("draft/functions.R"))
 
-redo_modals     <- T
+redo_modals     <- F
 skip_drive_auth <- F
 
 # authenticate to GoogleDrive using Google Service Account's secret JSON
